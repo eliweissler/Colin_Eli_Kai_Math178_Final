586d0c85ae2fdaff729d09bb3189420a03a2105b
Colin Adams <cadams@hmc.edu>
2020-06-02 11:41:27 -0700

    unhardcoded

--- classifiers/features.py
+++ classifiers/features.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline as IUS
 import scipy
-from sklearn.decomposition import PCA 
+from sklearn.decomposition import PCA
 
 def spline_accelerometer(feature_vec):
     """
@@ -18,7 +18,8 @@ def spline_accelerometer(feature_vec):
 
 
     #spline the accleration, aka alpha'
-    t = np.linspace(0, 2.54, 128) #define time interval
+    num = len(feature_vec)
+    t = np.arange(0,num) #define time interval
     a_x_spline = IUS(t, xline)
     a_y_spline = IUS(t, yline)
     a_z_spline = IUS(t, zline)
@@ -29,14 +30,14 @@ def spline_accelerometer(feature_vec):
 
 def calc_torsion(feature_vec):
     """
-    given accelerometer feature vector (which we call alpha prime), 
+    given accelerometer feature vector (which we call alpha prime),
     calculate the torsion at each 0.02 second time interval
 
     by finding first and second derivatives of accelerometer data
     """
     #get alpha'
     a_x_spline, a_y_spline, a_z_spline = spline_accelerometer(feature_vec)
-    
+
     #find alpha''
     ap_x_spline = a_x_spline.derivative()
     ap_y_spline = a_y_spline.derivative()
@@ -48,7 +49,8 @@ def calc_torsion(feature_vec):
     app_z_spline = ap_z_spline.derivative()
 
     #define time interval
-    t = np.linspace(0, 2.54, 128) 
+    num = len(feature_vec)
+    t = np.arange(0,num) #define time interval
 
     #calculate torsion
     torL = []
@@ -57,7 +59,7 @@ def calc_torsion(feature_vec):
         v_1 = np.array([a_x_spline(tt), a_y_spline(tt), a_z_spline(tt)])
         v_2 = np.array([ap_x_spline(tt), ap_y_spline(tt), ap_z_spline(tt)])
         v_3 = np.array([app_x_spline(tt), app_y_spline(tt), app_z_spline(tt)])
-        
+
         #calculate the torsion and shove it in list
         torsion = 3 * np.linalg.det([v_1, v_2, v_3])  \
                    /np.linalg.norm(np.cross(v_1, v_2))
@@ -68,29 +70,29 @@ def calc_torsion(feature_vec):
 
 def calc_curvature(feature_vec):
     """
-    given accelerometer feature vector (which we call alpha prime), 
+    given accelerometer feature vector (which we call alpha prime),
     calculate the curvature at each 0.02 second time interval
 
     by finding first and second derivatives of accelerometer data
     """
     #get alpha'
     a_x_spline, a_y_spline, a_z_spline = spline_accelerometer(feature_vec)
-    
+
     #find alpha''
     ap_x_spline = a_x_spline.derivative()
     ap_y_spline = a_y_spline.derivative()
     ap_z_spline = a_z_spline.derivative()
 
     #define time interval
-    t = np.linspace(0, 2.54, 128) 
-
+    num = len(feature_vec)
+    t = np.arange(0,num) #define time interval
     #calculate curvature
     curvL = []
     for tt in t:
         #calculate vector
         v_1 = np.array([a_x_spline(tt), a_y_spline(tt), a_z_spline(tt)])
         v_2 = np.array([ap_x_spline(tt), ap_y_spline(tt), ap_z_spline(tt)])
-        
+
         #calculate curvature
         curvature = 2 * np.linalg.norm(np.cross(v_1, v_2)) \
                     /np.linalg.norm(v_1)**(3/2)
@@ -101,20 +103,20 @@ def calc_curvature(feature_vec):
 def calc_fft(feature_vec, smooth = True):
     """
     takes in feature vector, splits it into its 3 components, and does the fft of each
-    returns magnitude of fft for each component 
+    returns magnitude of fft for each component
     """
     # separate components
     xline = feature_vec[0::3]
     yline = feature_vec[1::3]
     zline = feature_vec[2::3]
-    
-    # smooth the data, with sigma =2 
+
+    # smooth the data, with sigma =2
     if smooth:
         xline = scipy.ndimage.filters.gaussian_filter1d(xline,2)
         yline = scipy.ndimage.filters.gaussian_filter1d(yline,2)
         zline = scipy.ndimage.filters.gaussian_filter1d(zline,2)
 
-    
+
     # do fft of each
     xFFT = np.abs(np.fft.fftshift(np.fft.fft(xline)))
     yFFT = np.abs(np.fft.fftshift(np.fft.fft(yline)))
@@ -127,14 +129,11 @@ def calc_pca(feature_vec):
     takes in feature vector, returns eigenvectors and eigenvalues based on pca
     """
     # reshape the data
-    accXYZ = feature_vec.reshape(128,3)
+    accXYZ = feature_vec.reshape(len(feature_vec),3)
     pca = PCA(n_components = 3)
-        
+
     pca.fit(accXYZ)
     eigVals = pca.explained_variance_
     eigVects = pca.components_
 
     return eigVals, eigVects
-        
-    
-    
--- classifiers/mobiActRotateByGyro.py
+++ classifiers/mobiActRotateByGyro.py
@@ -19,38 +19,35 @@ def rotateFeatMats(featMat, savePath, fname):
     accFeats = [f for f in allFeats if 'a_' in f]
 
     gyroFeats = [f for f in allFeats if 'yaw_' in f or 'pitch_' in f or 'roll_' in f]
-    
+
     acc = featMat[accFeats].to_numpy()
     gyro = featMat[gyroFeats].to_numpy()
-    
-    rows = int(acc.shape[0]*128)
-    cols = int(acc.shape[1]/128)
-    
+
+    rows = int(acc.shape[0]*len(featMat[0]))
+    cols = int(acc.shape[1]/len(featMat[0]))
+
     accReshaped = acc.reshape(rows, cols)
     gyroReshaped = gyro.reshape(rows, cols)
-    
+
     # rotate acc by gyro data
     accRotated = rotate_to_zero(accReshaped, gyroReshaped)
-    
+
     accRotated = accRotated.reshape(acc.shape)
-    
+
     # rotate the rotated data by the pca axis
     rotPCAData, _ = np.apply_along_axis(PCA_rotate_data, 1, accRotated)
-    
+
     rotPCAData = np.concatenate(rotPCAData).reshape(acc.shape)
-    
+
     accDf = pd.DataFrame(rotPCAData, columns = accFeats)
     accDf.to_csv(os.path.join(savePath,fname), index = False)
-    
+
     return
 
 if __name__ == '__main__':
-    
+
     path = '/Users/kaikaneshina/Documents/MATH178/project_data/MobiAct_Dataset_v2.0/mobiAct_FeatMat.csv'
     featMat = pd.read_csv(path)
     savePath = '/Users/kaikaneshina/Documents/MATH178/project_data/MobiAct_Dataset_v2.0'
     fname = 'mobiAct_FeatMat_Rotated.csv'
     rotateFeatMats(featMat, savePath, fname)
-    
-    
-    
