046a96dcc97515e75150d94c4a60424e39868ae9
eliweissler <eliweissler@gmail.com>
2020-06-03 11:11:46 -0700

    finished updating wrapper

--- classifiers/crossvalidated_classifier.py
+++ classifiers/crossvalidated_classifier.py
@@ -14,6 +14,8 @@ from sklearn.ensemble import ExtraTreesClassifier
 from sklearn import svm
 from joblib import dump
 
+import os
+
 import pandas as pd
 import numpy as np
 
@@ -81,7 +83,8 @@ class Classifier:
 
         self.data.append(data_to_append)
 
-    def crossval(self, split_col = 'user', cols = None, col_score_split=['user','label']):
+    def crossval(self, split_col = 'user', cols = None, col_score_split=['user','label'],
+                 n_jobs = 1):
         """
         Creates a crossvalidated classifier
 
@@ -95,10 +98,14 @@ class Classifier:
             
         col_score_split: list of str
             list of columns to calculate the score breakdown on
+        
+        n_jobs: int
+            number of cores to give to sklearn. Colin set to 2, eli and kai
+            with tiny computers set to 1
 
         Returns
         -------
-        None.
+        a dictionary of accuracy breakdowns by different categories
 
         """
         #concatenate all of the data together
@@ -131,7 +138,7 @@ class Classifier:
         
         preds = cross_val_predict(estimator=self.model,
                                   X=X, y=y, groups=groups,
-                                  cv=cv, n_jobs=2)
+                                  cv=cv, n_jobs=n_jobs)
 
         # scores are in the order of the groups, so the first row out is the
         # result of training on the other groups, and testing on the first group
@@ -158,7 +165,8 @@ class Classifier:
 
 
 
-def wrapper(path_in, split_col, savePath, col_score_split=['user','label']):
+def wrapper(path_in, split_col, savePath, col_score_split=['user','label'],
+            n_jobs = 1):
     """
     wrapper for cross val classifier: applies 3 models, to the accerleration, and gyroscope data
 
@@ -172,10 +180,13 @@ def wrapper(path_in, split_col, savePath, col_score_split=['user','label']):
         where to save the csv
     col_score_split: list of str
        list of columns to calculate the score breakdown on
-
+    n_jobs: int
+            number of cores to give to sklearn. Colin set to 2, eli and kai
+            with tiny computers set to 1
     Returns
     -------
-    None.
+    1) list of score breakdown dataframes
+    2) list of predictions for each model 
 
     """
     if isinstance(path_in, str):
@@ -200,8 +211,12 @@ def wrapper(path_in, split_col, savePath, col_score_split=['user','label']):
     modelNames = ['k-NN', 'extra-Trees']#, 'SVC']
     
     scoreDf_list = [pd.DataFrame() for x in col_score_split]
+    preds_list = []
     for i_col, col in enumerate(col_score_split):
-        scoreDf_list[i_col][col] = list(data[0][col].unique())+['mean','std']
+        unique_vals = []
+        for d_set in data:
+            unique_vals = unique_vals + list(d_set[col].unique())
+        scoreDf_list[i_col][col] = list(np.unique(unique_vals))+['mean','stdev']
     
     for idx, model in enumerate(modelList):
         clf = Classifier(model)
@@ -211,14 +226,19 @@ def wrapper(path_in, split_col, savePath, col_score_split=['user','label']):
             clf.load_data(d_set, acc_feats)
             
         scores = clf.crossval(split_col=split_col,col_score_split=col_score_split)
+        preds_list.append(clf.preds)
         for i_col, col in enumerate(col_score_split):
             accuracy = scores[col]['accuracy'].values
             scoreDf_list[i_col][modelNames[idx]] = list(accuracy)+[np.mean(accuracy), np.std(accuracy)]
     
     for i in range(len(scoreDf_list)):
-        scoreDf_list[i].to_csv(savePath+"_"+col_score_split[i]+'.csv', index = False)
+        scoreDf_list[i].to_csv(savePath+"_accuracy_"+col_score_split[i]+'.csv', index = False)
     
-    return scoreDf_list
+    for i,m_name in enumerate(modelNames):
+        np.savetxt(savePath+"_pred_"+m_name+'.csv',preds_list[i],fmt="%s")
+    
+    
+    return scoreDf_list, preds_list
     
 if __name__ == "__main__":
     
@@ -267,14 +287,38 @@ if __name__ == "__main__":
     # save_path = '/Users/kaikaneshina/Documents/GitHub/Colin_Eli_Kai_Math178_Final/results/motionSense/userCrossVal/rotated/rotated128.csv'
     
     # data_path = '/Volumes/GoogleDrive/My Drive/Harvey Mudd/Work/Summer 2020/project_data/Feature_Matrix_128/MotionSense_FeatMat_Rotated.csv'
-    data_path = '/Volumes/GoogleDrive/My Drive/Harvey Mudd/Work/Summer 2020/project_data/Feature_Matrix_128/MotionSense_FeatMat.csv'
-    data = pd.read_csv(data_path)
-    save_path = '/Volumes/GoogleDrive/My Drive/Harvey Mudd/Work/Summer 2020/Colin_Eli_Kai_Math178_Final/results/motionSense/normal_split_by_activity/accuracy'
+    
     
     # # mobiact: regular
     # data_path = '/Users/kaikaneshina/Documents/MATH178/project_data/MobiAct_Dataset_v2.0/mobiAct_FeatMat.csv'
     # save_path = '/Users/kaikaneshina/Documents/GitHub/Colin_Eli_Kai_Math178_Final/results/mobiAct/userCrossVal/raw/raw128.csv'
     # # mobiact: rotated
     # # data_path = '/Users/kaikaneshina/Documents/MATH178/project_data/MobiAct_Dataset_v2.0/mobiAct_FeatMat_Rotated.csv'
-    scoreDf_list = wrapper(data_path, 'user', save_path,['user','label'])
+    
+    #colin set this to 2, eli/kai to 1
+    n_jobs = 1
+    
+    ms_normal = pd.read_csv('/Volumes/GoogleDrive/My Drive/Harvey Mudd/Work/Summer 2020/project_data/Feature_Matrix_128/MotionSense_FeatMat.csv')
+    ma_normal = pd.read_csv('/Volumes/GoogleDrive/My Drive/Harvey Mudd/Work/Summer 2020/project_data/Feature_Matrix_128/mobiAct_FeatMat.csv')
+    
+   
+    configs128 = {'motionSense_normal': {'data':ms_normal,'cv_col':'user',
+                                         'save_path' : '/Volumes/GoogleDrive/My Drive/Harvey Mudd/Work/Summer 2020/Colin_Eli_Kai_Math178_Final/results/motionSense/',
+                                         'splits':['user', 'label']}
+        }
+    
+    cross_dataset = {'cross_dataset_normal': {'data': [ms_normal,ma_normal],'cv_col':'dataset',
+                                              'save_path' : '/Volumes/GoogleDrive/My Drive/Harvey Mudd/Work/Summer 2020/Colin_Eli_Kai_Math178_Final/results/combined_dfs/',
+                                              'splits':['user','label','dataset']}
+        }
+    
+    config = cross_dataset
+    for setting in config:
+        data = config[setting]['data']
+        cv_col = config[setting]['cv_col']
+        save_path = config[setting]['save_path']+setting
+        os.makedirs(config[setting]['save_path'], exist_ok=True)
+        splits = config[setting]['splits']
+        scoreDf_list, preds_list = wrapper(data, cv_col, 
+                                           save_path,splits,n_jobs)
 
