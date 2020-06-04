2c02bd45eff10cd2ec7d48349e76e2c8dbb3afa5
Kai Kaneshina <kkaneshina@hmc.edu>
2020-06-03 22:06:48 -0700

    fixed orientation images

--- visualizing_data/orientation_and_rotation_plots.py
+++ visualizing_data/orientation_and_rotation_plots.py
@@ -28,6 +28,8 @@ from classifiers.mobiActRotateByGyro import rotateFeatMats
 
 from featMatHelpers import getAcc, getYPR
 
+import os
+
 matplotlib.rc('font', size=12)
 
 
@@ -65,7 +67,7 @@ def plot_on_sphere(xyz_data, normalize=False):
         
     return fig, ax
 
-def plot_adjusted_vs_unadjusted(featVec,n_entries=256,freq=50,nonlinear=False):
+def plot_adjusted_vs_unadjusted(featVec, savePath, n_entries=256,freq=50,nonlinear=False):
     """
     plots the adjusted and unadjusted acceleration data. FeatVec must be
     a dataframe of length 1, i.e. gotten by FeatMat.iloc[[i]]
@@ -76,36 +78,39 @@ def plot_adjusted_vs_unadjusted(featVec,n_entries=256,freq=50,nonlinear=False):
     times = np.arange(n_entries)*(1/freq)
     
     #rotate and align
-    acc_feats = ['a_' in c for c in featVec.columns]
     aligned, rotated, new_coords = rotateFeatMats(featVec, savePath=None, fname=None, featLen = 256)
     
     new_coords = new_coords[0]
     
-    acc = featVec[acc_feats].to_numpy().reshape(256,3)
-    acc_rotated = rotated.to_numpy().reshape(256,3)
-    acc_align = aligned.to_numpy().reshape(256,3)
+    acc = getAcc(featVec)
+    acc_rotated = rotated.reshape(n_entries, 3)
+    acc_align = getAcc(aligned)
     
-    f, ax = plt.subplots(nrows=1,ncols=3)
+    # obtain save info:
+    act = featVec.label.values[0]
+    
+    f, ax = plt.subplots(nrows=3,ncols=1, figsize=(25,15) )
 
     ax[0].plot(times,acc)
-    ax[0].legend(['a_x', 'a_y', 'a_z','norm'])
+    ax[0].legend(['a_x', 'a_y', 'a_z'])
     ax[0].set_xlabel('time (s)')
     ax[0].set_ylabel('acceleration (m/s^2)')
     ax[0].set_title('un-adjusted')
     
     ax[1].plot(times,acc_rotated)
-    ax[1].legend(['a_x', 'a_y', 'a_z','norm'])
+    ax[1].legend(['a_x', 'a_y', 'a_z'])
     ax[1].set_xlabel('time (s)')
     ax[1].set_ylabel('acceleration (m/s^2)')
     ax[1].set_title('rotated')
     
     ax[2].plot(times,acc_align)
-    ax[2].legend(['a_x', 'a_y', 'a_z','norm'])
+    ax[2].legend(['a_x', 'a_y', 'a_z'])
     ax[2].set_xlabel('time (s)')
     ax[2].set_ylabel('acceleration (m/s^2)')
     ax[2].set_title('rotated and aligned')
+    f.tight_layout(pad=2)
+    plt.savefig(os.path.join(savePath, act + '_all.png'))
     
-    f.set_size_inches((10,5))
     
     plt.figure()
     ax = plt.axes(projection='3d')
@@ -115,28 +120,44 @@ def plot_adjusted_vs_unadjusted(featVec,n_entries=256,freq=50,nonlinear=False):
     ax.set_zlabel('z')
     
     mean_a = np.mean(acc_rotated, axis=0)
-    ax.quiver(mean_a[0],mean_a[1],mean_a[2],new_coords[0,0],new_coords[0,1],new_coords[0,2], color = 'r')
-    ax.quiver(mean_a[0],mean_a[1],mean_a[2],new_coords[1,0],new_coords[1,1],new_coords[1,2], color = 'b')
-    ax.quiver(mean_a[0],mean_a[1],mean_a[2],new_coords[2,0],new_coords[2,1],new_coords[2,2], color = 'g')
+    ax.quiver(mean_a[0],mean_a[1],mean_a[2],new_coords[0][0],new_coords[0][1],new_coords[0][2], color = 'r')
+    ax.quiver(mean_a[0],mean_a[1],mean_a[2],new_coords[1][0],new_coords[1][1],new_coords[1][2], color = 'b')
+    ax.quiver(mean_a[0],mean_a[1],mean_a[2],new_coords[2][0],new_coords[2][1],new_coords[2][2], color = 'g')
     ax.set_title('rotated')
+    plt.savefig(os.path.join(savePath, act + '_rotatedVectors.png'))
+
     
     return 
 
 
 
-def plot_activity_heading(featMat,activity,n_entries=256):
+def plot_activity_heading(featMat,activity, savePath, n_entries=256, downsample = True):
     """
     For each observation plots on a sphere the point defined by the vector
     [1/sqrt(3), 1/sqrt(3), 1/sqrt(3)]
     in the coordinates defined by the roll,pitch, and yaw
     
-
+    plots the roll pitch and yaw data based on the x,y,z basis vectors
+    shows the phone's orientation
+    
+    single x,y,z are where the heading is pointing in the direction 
+    
     Returns
     -------
     None.
 
     """
+    # dictionary to translate from activity
+    d = {'wlk': 'Walking', 'sit': 'Sitting', 'std': 'Standing', 'ups': 'Walking Up Stairs',
+         'dws': 'Walking Down Stairs', 'jog': 'Jogging'}
     
+    # grab the activity of interest
+    featMat = featMat[featMat['label']==activity]
+    
+    # downsample the data so it doesn't take forever to run
+    if downsample:
+        featMat = featMat.loc[::50,:]
+
     gyro = getYPR(featMat)
 
     trans = R.from_euler('zxy',gyro).as_quat() #returns the scalar last
@@ -146,15 +167,83 @@ def plot_activity_heading(featMat,activity,n_entries=256):
     y = np.array([rotate_quat(Quaternion(imaginary = [0,1,0]), rot).imaginary for rot in trans])
     z = np.array([rotate_quat(Quaternion(imaginary = [0,0,1]), rot).imaginary for rot in trans])
     
+    # calculate mean / stdev of angles
+    # we know that r = 1, as the data is normalized.
+    phi = np.rad2deg(np.arctan2(z[:,1], z[:,0]))
+    theta = np.rad2deg(np.arccos(z[:,2]))
+    
     heading_vec = (x+y+z)/np.sqrt(3)
     
-    plot_on_sphere(heading_vec, normalize=False)
+    fig, ax = plot_on_sphere(heading_vec, normalize=False)
+    ax.set_title('Activity: ' + d[activity])
+    ax.set_xlabel('X', labelpad=10)    
+    ax.set_ylabel('Y', labelpad=10)
+    ax.set_zlabel('Z', labelpad=10)
     
-    return
+    plt.savefig(os.path.join(savePath, activity + '.png'))
     
+    fig, ax = plot_on_sphere(z, normalize=False)
+    ax.set_title('Activity: ' + d[activity])
+    ax.set_xlabel('X', labelpad=10)    
+    ax.set_ylabel('Y', labelpad=10)
+    ax.set_zlabel('Z', labelpad=10)
     
+    plt.savefig(os.path.join(savePath, activity + '_z.png'))
+    
+    return phi, theta
     
     
 
 if __name__ == '__main__':
-   x=1
+   
+    motionSense = '/Users/kaikaneshina/Documents/MATH178/project_data/featMat256/MotionSense_FeatMat_256.csv'
+    mobiAct = '/Users/kaikaneshina/Documents/MATH178/project_data/featMat256/mobiAct_FeatMat_256.csv'
+    
+    savePath = '/Users/kaikaneshina/Documents/MATH178/project_data/results/256/motionSense'
+    
+    df = pd.read_csv(motionSense)
+    
+    
+    # users = df.user.unique()[:5]
+    # for u in users:
+    #     dfSub = df[df['user']==u]
+    #     savePathSub = os.path.join(savePath,u)
+    #     try: os.mkdir(savePathSub)
+    #     except: pass
+        
+    #     phiList = []
+    #     thetaList = []
+    #     activities = dfSub.label.unique()
+    #     for act in activities:
+    #         phi, theta = plot_activity_heading(dfSub,act, savePathSub)
+    #         phiList.append( [np.mean(phi), np.std(phi)])
+    #         thetaList.append( [np.mean(theta), np.std(theta)])
+                         
+    #     theta = np.array(thetaList)
+    #     phi = np.array(phiList)
+        
+    #     angles = pd.DataFrame()
+    #     angles['activity'] = activities
+    #     angles['theta mean'] = theta[:,0]
+    #     angles['theta stdev'] = theta[:,1]
+    #     angles['phi mean'] = phi[:,0]
+    #     angles['phi stdev'] = phi[:,1]
+    
+    #     angles.to_csv(os.path.join(savePathSub, 'angles.csv'), index = False)
+    
+    activities = df.label.unique()
+    savePath = os.path.join(savePath, 'rotation')
+    try: os.mkdir(savePath)
+    except: pass
+    for act in activities:
+        subDf = df[df['label']==act]
+        featVect= subDf.iloc[0,:].to_frame().T
+        plot_adjusted_vs_unadjusted(featVect, savePath)
+    
+    
+    
+    
+    
+    
+   
+    
